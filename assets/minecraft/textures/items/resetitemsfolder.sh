3bc5d2f016c9839bad2c1cb370255067c052486b
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2023-07-29 08:06:46 -0400

    Update resetitemsfolder.sh

--- assets/minecraft/textures/items/resetitemsfolder.sh
+++ assets/minecraft/textures/items/resetitemsfolder.sh
@@ -4,3 +4,11 @@ RM -F *_axe.png
 RM -F *_sword.png
 RM -F bow_*.png
 RM -F fishing_rod_*.png
+RM -F apple.png
+RM -F *_cooked.png
+RM -F *_raw.png
+RM -F bread.png
+RM -F carro*.png
+RM -F melo*.png
+RM -F potat*.png
+RM -F pumpkin_pie.png
