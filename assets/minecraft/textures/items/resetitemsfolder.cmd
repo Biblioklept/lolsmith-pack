ff0d593d041b6fbb6653e13dd1fe51650ab80e45
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2023-07-29 08:06:09 -0400

    Foodstuffs update.

--- assets/minecraft/textures/items/resetitemsfolder.cmd
+++ assets/minecraft/textures/items/resetitemsfolder.cmd
@@ -4,3 +4,11 @@ DEL *_axe.png
 DEL *_sword.png
 DEL bow_*.png
 DEL fishing_rod_*.png
+DEL apple.png
+DEL *_cooked.png
+DEL *_raw.png
+DEL bread.png
+DEL carro*.png
+DEL melo*.png
+DEL potat*.png
+DEL pumpkin_pie.png
